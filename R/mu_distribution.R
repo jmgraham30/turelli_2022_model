61691c38116d5e6e12856a35c37acb192e3d2ebb
Jason M Graham <prof.jason.m.graham@gmail.com>
2024-02-22 15:54:22 -0500

    update

---
+++ R/mu_distribution.R
@@ -0,0 +1,17 @@
+library(tidyverse)
+library(gamlss.inf)
+
+?BEINF
+
+mu_sample <- tibble(x = rBEINF(1000, mu = 0.2, sigma = 0.9, nu = 0.9)) |>
+  mutate(x = round(x,2))
+
+mu_sample |> 
+  ggplot(aes(x)) +
+  geom_histogram(binwidth=0.05,fill = "black", color = "white") +
+  theme_bw() + 
+  xlim(c(-0.2,1.1))
+
+summary(mu_sample)
+
+View(mu_sample)
