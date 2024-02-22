5880039d431c08b425061604679da1f894d4b280
Jason M Graham <prof.jason.m.graham@gmail.com>
2024-02-22 18:31:56 -0500

    update

--- R/plot_mu_model_data.R
+++ R/plot_mu_model_data.R
@@ -16,7 +16,7 @@ mu_model_data |>
                 labels = trans_format("log10", math_format(10^.x)))
 
 mu_model_data_avgs <- mu_model_data |>
-  group_by(sh_val,F_val_m,mu_m) |>
+  group_by(sh_val,F_val_m) |>
   summarise(t_f_avg = mean(t_f), t_f_sd = sd(t_f), n = n()) 
 
 mu_model_data_avgs |>
@@ -32,22 +32,19 @@ mu_model_data_avgs |>
                 labels = trans_format("log10", math_format(10^.x)))
 
 mu_model_data_avgs |>
-  mutate(sh_val = as.factor(sh_val)) |>
-  ggplot(aes(x = F_val_m, y = mu_m, fill = t_f_avg)) +
+  ggplot(aes(x = F_val_m, y = sh_val, fill = t_f_avg)) +
   geom_tile() +
   scale_fill_gradient(trans = "log10",low = "lightblue", high = "darkblue",
                       breaks = trans_breaks("log10", function(x) 10^x),
                       labels = trans_format("log10", math_format(10^.x))) +
-  facet_wrap(~sh_val) + 
-  labs(x = TeX("$Median_{F}$"), y = TeX("$Median_{\\mu}$"), fill = TeX("$\\hat{E}(T_{Loss})$")) 
+  labs(x = TeX("$F$"), y = TeX("$s_h$"), fill = TeX("$\\hat{E}(T_{Loss})$")) 
 
 
 mu_model_data_avgs |>
   mutate(sh_val = as.factor(sh_val)) |>
-  ggplot(aes(x = F_val_m, y = mu_m, fill = t_f_sd)) +
+  ggplot(aes(x = F_val_m, y = sh_val, fill = t_f_sd)) +
   geom_tile() +
   scale_fill_gradient(trans = "log10",low = "lightblue", high = "darkblue",
                       breaks = trans_breaks("log10", function(x) 10^x),
                       labels = trans_format("log10", math_format(10^.x))) +
-  facet_wrap(~sh_val) + 
-  labs(x = TeX("$Median_{F}$"), y = TeX("$Median_{\\mu}$"), fill = TeX("$\\hat{\\sigma}(T_{Loss})$")) 
+  labs(x = TeX("$F$"), y = TeX("$s_h$"), fill = TeX("$\\hat{\\sigma}(T_{Loss})$")) 
