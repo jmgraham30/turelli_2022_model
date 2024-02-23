1610f7c491f6f7b3634ee19e8bf0980efb6f4ff3
Jason M Graham <prof.jason.m.graham@gmail.com>
2024-02-22 19:27:41 -0500

    update

--- R/plot_mu_model_data.R
+++ R/plot_mu_model_data.R
@@ -11,9 +11,10 @@ glimpse(mu_model_data)
 
 mu_model_data |>
   ggplot(aes(x = t_f)) + 
-  geom_histogram(color="white",fill="steelblue") + 
-  scale_x_log10(breaks = trans_breaks("log10", function(x) 10^x),
-                labels = trans_format("log10", math_format(10^.x)))
+  geom_histogram(color="white",fill="steelblue") 
+
+#  scale_x_log10(breaks = trans_breaks("log10", function(x) 10^x),
+#                labels = trans_format("log10", math_format(10^.x)))
 
 mu_model_data_avgs <- mu_model_data |>
   group_by(sh_val,F_val_m) |>
@@ -21,30 +22,33 @@ mu_model_data_avgs <- mu_model_data |>
 
 mu_model_data_avgs |>
   ggplot(aes(x = t_f_avg)) + 
-  geom_histogram(color="white",fill="steelblue") + 
-  scale_x_log10(breaks = trans_breaks("log10", function(x) 10^x),
-                labels = trans_format("log10", math_format(10^.x)))
+  geom_histogram(color="white",fill="steelblue") 
+
+#  scale_x_log10(breaks = trans_breaks("log10", function(x) 10^x),
+#             labels = trans_format("log10", math_format(10^.x)))
 
 mu_model_data_avgs |>
   ggplot(aes(x = t_f_sd)) + 
-  geom_histogram(color="white",fill="steelblue") + 
-  scale_x_log10(breaks = trans_breaks("log10", function(x) 10^x),
-                labels = trans_format("log10", math_format(10^.x)))
+  geom_histogram(color="white",fill="steelblue") 
+
+#  scale_x_log10(breaks = trans_breaks("log10", function(x) 10^x),
+#                labels = trans_format("log10", math_format(10^.x)))
 
 mu_model_data_avgs |>
   ggplot(aes(x = F_val_m, y = sh_val, fill = t_f_avg)) +
   geom_tile() +
-  scale_fill_gradient(trans = "log10",low = "lightblue", high = "darkblue",
-                      breaks = trans_breaks("log10", function(x) 10^x),
-                      labels = trans_format("log10", math_format(10^.x))) +
+  scale_fill_gradient(low = "lightblue", high = "darkblue") +
+ # scale_fill_gradient(trans = "log10",low = "lightblue", high = "darkblue",
+ #                  breaks = trans_breaks("log10", function(x) 10^x),
+ #                  labels = trans_format("log10", math_format(10^.x))) +
   labs(x = TeX("$F$"), y = TeX("$s_h$"), fill = TeX("$\\hat{E}(T_{Loss})$")) 
 
 
 mu_model_data_avgs |>
-  mutate(sh_val = as.factor(sh_val)) |>
   ggplot(aes(x = F_val_m, y = sh_val, fill = t_f_sd)) +
   geom_tile() +
-  scale_fill_gradient(trans = "log10",low = "lightblue", high = "darkblue",
-                      breaks = trans_breaks("log10", function(x) 10^x),
-                      labels = trans_format("log10", math_format(10^.x))) +
+  scale_fill_gradient(low = "lightblue", high = "darkblue") +
+#  scale_fill_gradient(trans = "log10",low = "lightblue", high = "darkblue",
+#                      breaks = trans_breaks("log10", function(x) 10^x),
+#                      labels = trans_format("log10", math_format(10^.x))) +
   labs(x = TeX("$F$"), y = TeX("$s_h$"), fill = TeX("$\\hat{\\sigma}(T_{Loss})$")) 
